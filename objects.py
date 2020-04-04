c816f782b53113e5f29752776550820d5d43245c
Ekaterina Korobova <katekorobova@yandex.ru>
2020-04-04 17:25:38 +0300

    Загрузка уровней

---
+++ objects.py
@@ -0,0 +1,10 @@
+class Player:
+    def __init__(self, x: int, y: int):
+        self.x = x
+        self.y = y
+
+
+class Box:
+    def __init__(self, x: int, y: int):
+        self.x = x
+        self.y = y
