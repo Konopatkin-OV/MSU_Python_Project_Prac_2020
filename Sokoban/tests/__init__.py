bc936b580b09a012c57d288d7a43b6b6f2f72101
Ekaterina Korobova <katekorobova@yandex.ru>
2020-05-20 22:04:45 +0300

    / -> join

--- Sokoban/tests/__init__.py
+++ Sokoban/tests/__init__.py
@@ -1,6 +1,7 @@
 import unittest
+import os
 
 loader = unittest.TestLoader()
-tests = loader.discover('Sokoban/tests')
+tests = loader.discover(os.path.join('Sokoban', 'tests'))
 testRunner = unittest.TextTestRunner()
 testRunner.run(tests)
