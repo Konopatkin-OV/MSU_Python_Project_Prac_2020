4777b35b4b2afdd5511eb71dc6892725d74a57ae
Ekaterina Korobova <katekorobova@yandex.ru>
2020-05-22 21:45:00 +0300

    Tests fixed

--- Sokoban/tests/__init__.py
+++ Sokoban/tests/__init__.py
@@ -2,6 +2,6 @@ import unittest
 import os
 
 loader = unittest.TestLoader()
-tests = loader.discover(os.path.join('Sokoban', 'tests'))
+tests = loader.discover(os.path.dirname(__file__))
 testRunner = unittest.TextTestRunner()
 testRunner.run(tests)
