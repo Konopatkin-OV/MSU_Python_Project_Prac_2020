3faf0b8347be04a1f716e5a6cc2d85ed20f46f45
Олег Конопаткин <s15b1_konopatkin@179.ru>
2020-05-23 15:24:30 +0300

    fixed flake8 in setup.py

--- setup.py
+++ setup.py
@@ -18,7 +18,8 @@ setup(
 
     package_data={
         # If any package contains *.txt or *.rst files, include them:
-        "Sokoban": ["*.rst", "*.po", os.path.join("images", "*.png"), os.path.join("levels", "*.lvl")],
+        "Sokoban": ["*.rst", "*.po",
+                    os.path.join("images", "*.png"), os.path.join("levels", "*.lvl")],
         # And include any *.msg files found in the "hello" package, too:
         # "hello": ["*.msg"],
     },
